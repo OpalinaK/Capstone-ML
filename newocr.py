ca8aa6a72722bcb917f522a5368a034a321ad886
danielkim1129 <125942385+danielkim1129@users.noreply.github.com>
2025-03-30 17:57:16 -0400

    Update newocr.py

--- newocr.py
+++ newocr.py
@@ -2,13 +2,73 @@ import cv2
 import easyocr
 from ultralytics import YOLO
 
+
 model = YOLO("best.pt")
 reader = easyocr.Reader(['en'])
 
-image_path = "image2.jpg"
+image_path = "image4.jpg"
 image = cv2.imread(image_path)
 directions = []
 full_text_results = reader.readtext(image)
+
+# Grouping
+grouped_texts = []
+final_group = []
+horizontal_threshold = 250  # Adjust for horizontal proximity
+vertical_threshold = 30  # Allow slight vertical alignment
+
+for i, (bbox1, text1, conf1) in enumerate(full_text_results):
+    (x1, y1), (x2, y2) = bbox1[0], bbox1[2]
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    merged = False
+    for group in grouped_texts:
+        for _, (gx1, gy1, gx2, gy2), _ in group:
+            horizontal_distance = abs(x1 - gx2)  # Focus on right edge of previous box
+            vertical_distance = abs((y1 + y2) / 2 - (gy1 + gy2) / 2)
+
+            if horizontal_distance < horizontal_threshold and vertical_distance < vertical_threshold:
+                group.append((text1, (x1, y1, x2, y2), conf1))
+                merged = True
+                break
+        if merged:
+            break
+    if not merged:
+        grouped_texts.append([(text1, (x1, y1, x2, y2), conf1)])
+
+for group in grouped_texts:
+    x1s, y1s, x2s, y2s = [], [], [], []
+    for _, (x1, y1, x2, y2), _ in group:
+        x1s.append(x1)
+        y1s.append(y1)
+        x2s.append(x2)
+        y2s.append(y2)
+
+    gx1, gy1, gx2, gy2 = int(min(x1s)), int(min(y1s)), int(max(x2s)), int(max(y2s))
+    cv2.rectangle(image, (gx1, gy1), (gx2, gy2), (0, 0, 255), 2)  # Red box for grouped text
+
+    grouped_text = " ".join([word[0] for word in group])
+    final_group.append(grouped_text)
+
+for g in final_group:
+    print(g)
+    if 'Gates' not in g:
+        print("remove")
+        final_group.remove(g)
+
+if len(final_group) == 0:
+    print("No Gates Info")
+    exit()
+
+
+
+# Destination = 'A'
+# for g in final_group:
+#     if 'A' in g:
+
+
+
+
+
 
 for (_, text, confidence) in full_text_results:
     if "Gate" in text:
